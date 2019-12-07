ce4f27f23f9b91e984e923da7fe711e9fccb294f
mattladany <matt@va-a.com>
2019-12-06 23:16:51 -0500

    Code clean up

--- 2019/12-05/intcode-2.0.rb
+++ 2019/12-05/intcode-2.0.rb
@@ -1,103 +1,94 @@
 #!/usr/bin/env ruby
 
+# Executes the provided Intcode.
+# 
+# @param [Integer[]] intcode The Intcode being processed.
 def intcode_executor(intcode)
 
     index = 0
     while index < intcode.length
 
+        # Get the optcode for this instruction.
         optcode = intcode[index] % 100
 
+        # Short-circuit if the program should exit.
         break if optcode == 99
 
-        if optcode == 1
-            param1_type = intcode[index] / 100 % 10
-            param1_value = getParamValue(param1_type, intcode, index+1)
-            param2_type = intcode[index] / 1000 % 10
-            param2_value = getParamValue(param2_type, intcode, index+2)
+        # Get the paramter values (in case they are needed).
+        param1_value = getParamValue(intcode[index] / 100 % 10, intcode, index+1)
+        param2_value = getParamValue(intcode[index] / 1000 % 10, intcode, index+2)
 
+        case optcode
+
+        # Add parameter values.
+        when 1
             intcode[intcode[index+3]] = param1_value + param2_value
             index += 4
 
-        elsif optcode == 2
-            param1_type = intcode[index] / 100 % 10
-            param1_value = getParamValue(param1_type, intcode, index+1)
-            param2_type = intcode[index] / 1000 % 10
-            param2_value = getParamValue(param2_type, intcode, index+2)
-
+        # Multiply parameter values.
+        when 2
             intcode[intcode[index+3]] = param1_value * param2_value
             index += 4
 
-        elsif optcode == 3
+        # Save the input value to a location.
+        when 3
             puts "Enter 1 to continue, 0 to exit"
-            input = 5
-            intcode[intcode[index+1]] = input
+            input = gets.chomp
+            intcode[intcode[index+1]] = input.to_i
             index += 2
 
-        elsif optcode == 4
-            param1_type = intcode[index] / 100 % 10
-            param1_value = getParamValue(param1_type, intcode, index+1)
+        # Print the parameter value.
+        when 4
             puts param1_value
             index += 2
 
-        elsif optcode == 5
-            param1_type = intcode[index] / 100 % 10
-            param1_value = getParamValue(param1_type, intcode, index+1)
-            param2_type = intcode[index] / 1000 % 10
-            param2_value = getParamValue(param2_type, intcode, index+2)
-
+        # Jump if true.
+        when 5
             if param1_value != 0
                 index = param2_value
             else
                 index += 3
             end
 
-        elsif optcode == 6
-            param1_type = intcode[index] / 100 % 10
-            param1_value = getParamValue(param1_type, intcode, index+1)
-            param2_type = intcode[index] / 1000 % 10
-            param2_value = getParamValue(param2_type, intcode, index+2)
-
+        # Jump if false.
+        when 6
             if param1_value == 0
                 index = param2_value
             else
                 index += 3
             end
-            
-        elsif optcode == 7
-            param1_type = intcode[index] / 100 % 10
-            param1_value = getParamValue(param1_type, intcode, index+1)
-            param2_type = intcode[index] / 1000 % 10
-            param2_value = getParamValue(param2_type, intcode, index+2)
 
+        # Check if less than.
+        when 7
             if param1_value < param2_value
                 intcode[intcode[index+3]] = 1
             else
                 intcode[intcode[index+3]] = 0
             end
-            
             index += 4
 
-        elsif optcode == 8
-            param1_type = intcode[index] / 100 % 10
-            param1_value = getParamValue(param1_type, intcode, index+1)
-            param2_type = intcode[index] / 1000 % 10
-            param2_value = getParamValue(param2_type, intcode, index+2)
-
+        # Check if equals.
+        when 8
             if param1_value == param2_value
                 intcode[intcode[index+3]] = 1
             else
                 intcode[intcode[index+3]] = 0
             end
-
             index +=4
-
         else
             raise "Uh oh...bad optcode"
         end
     end
-   
 end
 
+# Gets a particular parameter value from the intcode provided.
+# 
+# @param [Integer] param_type The type of the parameter.
+#   This can be one of two values:
+#       - 0 : if the parameter holds an address.
+#       - 1 : if the parameter holds a value.
+# @param [Integer[]] intcode The Intcode being processed.
+# @param [Integer] index The index the parameter information is located at.
 def getParamValue(param_type, intcode, index)
     if param_type == 0
         return intcode[intcode[index]]
@@ -108,7 +99,7 @@ def getParamValue(param_type, intcode, index)
     end
 end
 
-file = File.read("2019/12-05/input")
+file = File.read("input")
 
 intcode = file.split(',').map(&:to_i)
 
