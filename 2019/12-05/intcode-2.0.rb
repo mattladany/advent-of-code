ae0d5e62d772a46bb6221d159823a1a0be516f20
mattladany <matt@va-a.com>
2019-12-06 22:09:39 -0500

    Solved part 2 for 12-05

--- 2019/12-05/intcode-2.0.rb
+++ 2019/12-05/intcode-2.0.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 def intcode_executor(intcode)
-    
+
     index = 0
     while index < intcode.length
 
@@ -17,6 +17,7 @@ def intcode_executor(intcode)
 
             intcode[intcode[index+3]] = param1_value + param2_value
             index += 4
+
         elsif optcode == 2
             param1_type = intcode[index] / 100 % 10
             param1_value = getParamValue(param1_type, intcode, index+1)
@@ -25,16 +26,71 @@ def intcode_executor(intcode)
 
             intcode[intcode[index+3]] = param1_value * param2_value
             index += 4
+
         elsif optcode == 3
             puts "Enter 1 to continue, 0 to exit"
-            input = 1
+            input = 5
             intcode[intcode[index+1]] = input
             index += 2
+
         elsif optcode == 4
             param1_type = intcode[index] / 100 % 10
             param1_value = getParamValue(param1_type, intcode, index+1)
             puts param1_value
             index += 2
+
+        elsif optcode == 5
+            param1_type = intcode[index] / 100 % 10
+            param1_value = getParamValue(param1_type, intcode, index+1)
+            param2_type = intcode[index] / 1000 % 10
+            param2_value = getParamValue(param2_type, intcode, index+2)
+
+            if param1_value != 0
+                index = param2_value
+            else
+                index += 3
+            end
+
+        elsif optcode == 6
+            param1_type = intcode[index] / 100 % 10
+            param1_value = getParamValue(param1_type, intcode, index+1)
+            param2_type = intcode[index] / 1000 % 10
+            param2_value = getParamValue(param2_type, intcode, index+2)
+
+            if param1_value == 0
+                index = param2_value
+            else
+                index += 3
+            end
+            
+        elsif optcode == 7
+            param1_type = intcode[index] / 100 % 10
+            param1_value = getParamValue(param1_type, intcode, index+1)
+            param2_type = intcode[index] / 1000 % 10
+            param2_value = getParamValue(param2_type, intcode, index+2)
+
+            if param1_value < param2_value
+                intcode[intcode[index+3]] = 1
+            else
+                intcode[intcode[index+3]] = 0
+            end
+            
+            index += 4
+
+        elsif optcode == 8
+            param1_type = intcode[index] / 100 % 10
+            param1_value = getParamValue(param1_type, intcode, index+1)
+            param2_type = intcode[index] / 1000 % 10
+            param2_value = getParamValue(param2_type, intcode, index+2)
+
+            if param1_value == param2_value
+                intcode[intcode[index+3]] = 1
+            else
+                intcode[intcode[index+3]] = 0
+            end
+
+            index +=4
+
         else
             raise "Uh oh...bad optcode"
         end
